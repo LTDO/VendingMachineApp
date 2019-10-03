611ed69d21a53bcaf89180e1efe64eb9d849bb00
LTDO <lungatshila10@gmail.com>
2019-10-03 17:27:14 +0200

    Added two more products

--- RunMachine.java
+++ RunMachine.java
@@ -2,17 +2,24 @@ public class RunMachine{
 
     public static void main(String[] args) {
 
-
         VendingMachine product = new VendingMachine();
-//        SoftDrink coke = new SoftDrink("Sprite");
-//        product.addStock(coke, 0);
-//        product.buy(coke);
 
         //Chocolates
         Chocolate barOne = new Chocolate("Energy Bar");
-        product.addStock(barOne, 0);
-        product.buy(barOne);
+        product.addStock(barOne, 1);
         product.buy(barOne);
         System.out.println(barOne.getStock());
+
+        //SoftDrinks
+        SoftDrink coke = new SoftDrink("Coke");
+        product.addStock(coke, 1);
+        product.buy(coke);
+        System.out.println(coke.getStock());
+
+        //Salty Snacks
+        SaltySnack snack = new SaltySnack("Simba");
+        product.addStock(snack, 2);
+        product.buy(snack);
+        System.out.println(snack.getStock());
     }
 }
--- VendingMachine.java
+++ VendingMachine.java
@@ -1,10 +1,18 @@
 public class VendingMachine {
 
     public static int stockCount = 0;
-
     public void buy(Product product) throws ProductNotFoundException{
 
         if(stockCount <= 0){
+            if(product.equals(new ChocolateNotFoundException())){
+                throw new ChocolateNotFoundException();
+            }
+//            else if (product.equals(new SaltySnackNotFoundException())){
+//                throw new SaltySnackNotFoundException();
+//            }
+//            else if(product.equals(new SoftDrinkNotFoundException())){
+//                throw new SoftDrinkNotFoundException();
+//            }
         }else {
             stockCount--;
         }
