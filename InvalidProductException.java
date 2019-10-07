fd81b0392ce7786cdc85cb76fe8a56f0b7f3377e
LTDO <lungatshila10@gmail.com>
2019-10-07 12:24:14 +0200

    modified error message for InvalidProductException

--- InvalidProductException.java
+++ InvalidProductException.java
@@ -1,5 +1,2 @@
 public class InvalidProductException extends RuntimeException{
-    public InvalidProductException(){
-        System.out.println("Opp's your product is not available at this moment");
-    }
 }
--- RunMachine.java
+++ RunMachine.java
@@ -3,7 +3,7 @@ public class RunMachine{
 
         VendingMachine product = new VendingMachine();
 
-        //Chocolate
+        //Chocolates
         StringBuilder chocolates = new StringBuilder("CHOCOLATES");
         chocolates.append("\n" + "1. Lunch Bar: ");
         System.out.println(chocolates);
@@ -14,7 +14,7 @@ public class RunMachine{
         System.out.println(">>>>" + " " + product.getChocolateCount() + " chockies left");
         System.out.println();
 
-        //Salty Snack
+        //Salty Snacks
         StringBuilder saltySnacks = new StringBuilder("SALTY SNACKS");
         saltySnacks.append("\n" + "1. Pretzel");
         System.out.println(saltySnacks);
@@ -24,8 +24,7 @@ public class RunMachine{
         System.out.println(">>> " + product.getSaltySnackCount() + " snacks left");
         System.out.println();
 
-
-        //Soft Drink
+        //Soft Drinks
         StringBuilder buildSoftDrink = new StringBuilder("COOL DRINKS");
         buildSoftDrink.append("\n" + "1. Low sugar Sprite: ");
         System.out.println(buildSoftDrink);
@@ -36,16 +35,22 @@ public class RunMachine{
         System.out.println(">>> " + product.getSoftDrinkCount() + " drinks left");
         System.out.println();
 
+        //Total Number of products in stock.
         StringBuilder totProducts = new StringBuilder("Total stock for all Products");
         totProducts.append("\n").append(product.totalStockCount());
         System.out.println(totProducts);
+        System.out.println();
 
-        //Testing Invalid Product
-        StringBuilder builder = new StringBuilder("COOL DRINKS");
-        builder.append("\n" + "1. Zero Coke: ");
-        SoftDrink sprite = new SoftDrink("Zero Cocoa Cola");
-        product.buy(null);
-        System.out.println(builder);
-        System.out.print(sprite.totalStockCount());
+        //Testing Invalid Products
+        try {
+            StringBuilder builder = new StringBuilder("COOL DRINKS");
+            builder.append("\n" + "1. Zero Coke: ");
+            SoftDrink sprite = new SoftDrink("Zero Cocoa Cola");
+            product.buy(null);
+            System.out.println(builder);
+            System.out.print(sprite.totalStockCount());
+        }catch (InvalidProductException e){
+            System.out.println("ERROR: " +"\033[3mSpecified product not available\033[0m");
+        }
     }
 }
